e5020d398c00bb8c959a0815479b60c0254dd34f
kushtiwari13 <67438546+kushtiwari13@users.noreply.github.com>
2023-05-07 13:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  const gridItems = document.querySelectorAll('.grid-item');
+
+  document.getElementById('color-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const blockId = document.getElementById('block_id').value;
+    const color = document.getElementById('color_id').value;
+
+    gridItems.forEach(function(item) {
+      item.style.backgroundColor = 'transparent';
+    });
+
+    const selectedBlock = document.getElementById(blockId);
+    selectedBlock.style.backgroundColor = color;
+  });
+
+  document.getElementById('reset_button').addEventListener('click', function() {
+    gridItems.forEach(function(item) {
+      item.style.backgroundColor = 'transparent';
+    });
+  });
+});
